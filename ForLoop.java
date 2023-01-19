676ad05ddbe5c76d682fbc47edbbf562024d0615
immohanraj <mohanraju1203@gmail.com>
2023-01-19 17:58:05 +0530

    Done

---
+++ ForLoop.java
@@ -0,0 +1,27 @@
+package loop.org;
+
+public class ForLoop {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Start");
+		
+		for (int i =1; i <4; i++) {
+			
+			System.out.print("A"+ " ");
+			
+		}
+		System.out.println();
+		
+		System.out.println("end");
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
